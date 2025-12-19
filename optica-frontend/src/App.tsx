5696068f6bb3cc708b932558a5dfb5cd39526e76
santir9515 <santir9515@gmail.com>
2025-12-18 22:38:15 -0300

    se añade la ruta a insumos.tsx

--- optica-frontend/src/App.tsx
+++ optica-frontend/src/App.tsx
@@ -3,6 +3,7 @@ import AppLayout from "./layout/AppLayout";
 
 import ClientesList from "./pages/Clientes/ClientesList";
 import ProveedoresList from "./pages/Proveedores/ProveedoresList";
+import InsumosList from "./pages/Insumos/InsumosList";
 
 export default function App() {
   return (
@@ -11,7 +12,7 @@ export default function App() {
         <Route path="/" element={<Navigate to="/clientes" replace />} />
         <Route path="/clientes" element={<ClientesList />} />
         <Route path="/proveedores" element={<ProveedoresList />} />
-        {/* <Route path="/insumos" element={<InsumosList />} /> */}
+        <Route path="/insumos" element={<InsumosList />} />
         {/* <Route path="/compras-insumos" element={<ComprasList />} /> */}
         {/* <Route path="/pedidos-laboratorio" element={<PedidosList />} /> */}
         {/* <Route path="/recetas" element={<RecetasList />} /> */}
